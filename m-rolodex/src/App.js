fe1519e64c970aaf5efa71d797dd04e7450ad93e
Engel Noel Berihuete <emaxing23@gmail.com>
2025-02-02 13:27:03 -0500

    adding fuction map to render an array of names

--- m-rolodex/src/App.js
+++ m-rolodex/src/App.js
@@ -1,25 +1,48 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+
+       cartoons:[
+
+        {
+            name:'Linda',
+            id:'01',
+        },
+
+        {
+            name:'Mark',
+            id:'02',
+        },
+
+        {
+            name:'Jane',
+            id:'03',
+        },
+
+        {
+            name:'Petter',
+            id:'04',
+        },
+
+       ],   
+    
+    };
+  }
+
+  render() {
+    return <div className="App">
+        {this.state.cartoons.map((cartoon)=>{
+            return <div key={cartoon.id}><h1>{cartoon.name}</h1></div>;
+              
+        })}
+    </div>;
+  }
 }
 
 export default App;
